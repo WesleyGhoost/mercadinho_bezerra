4ea0606159bcc8453cadd4f36b528a64e873560e
Wesley <wesley.felix12@hotmail.com>
2025-04-21 11:07:12 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import './components/Header'
+import './components/About'
+import './components/Products'
+import './components/Footer'
+
+function App() {
+  return (
+  <>
+    <Header />
+    <About />
+    <Products />
+    <Footer />
+  </>
+  )
+}
+
+export default App
