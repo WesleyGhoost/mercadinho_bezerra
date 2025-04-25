3ee63b89c2627b18bfd93ef5092d11d9344c2b92
Wesley <wesley.felix12@hotmail.com>
2025-04-25 10:35:21 -0300

    feat: footer component

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,25 @@
+import './Footer.css'
+
+function Footer() {
+    return (
+        <footer className='footer-container'>
+            <div className="footer-content">
+                <div>
+                    <h4>Mercadinho Bezerra</h4>
+                    <p>CNPJ: 00.000.000/0000-00</p>
+                    <p>Desde 2019 servindo com carinho</p>
+                </div>
+
+                <div>
+                    <h4>Contato</h4>
+                    <p>Tel: (11) 99999-9999</p>
+                </div>
+            </div>
+            <div className="footer-bottom">
+                <p>&copy; 2025 Mercadinho Bezerra — Todos os direitos reservados.</p>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
